9fcf8ac207c0dfdb427a775464f240618999212a
Russell Anderson <russellanderson2000@gmail.com>
2020-02-03 23:02:45 -0500

    WIP: trying to pass dates picked to second activity

--- app/src/main/java/com/example/secondapp/DisplayTermInfoActivity.java
+++ app/src/main/java/com/example/secondapp/DisplayTermInfoActivity.java
@@ -1,14 +1,33 @@
 package com.example.secondapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class DisplayTermInfoActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_term_info);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView3);
+        textView.setText(message);
+//
+//        TextView textView4 = findViewById(R.id.textView4);
+//        TextView.setText(StartDate);
+//
+//        TextView textView6 = findViewById(R.id.textView6);
+//        TextView.setText(endDate);
+
     }
 }
+
--- app/src/main/java/com/example/secondapp/MainActivity.java
+++ app/src/main/java/com/example/secondapp/MainActivity.java
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.secondapp.MESSAGE";
+    DatePicker picker;
+    DatePicker picker2;
+    Button btnGet;
+    TextView tvw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +28,23 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
+
+        DatePicker datePicker1 = (DatePicker) findViewById(R.id.datePicker1);
+        DatePicker datePicker2 = (DatePicker) findViewById(R.id.datePicker2);
+        String startDate = datePicker1.toString();
+        String endDate = datePicker2.toString();
+
+//        btnGet.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                tvw.setText("Selected Date: "+ picker.getDayOfMonth()+"/"+ (picker.getMonth() + 1)+"/"+picker.getYear());
+//                tvw.setText("Selected Date: "+ picker2.getDayOfMonth()+"/"+ (picker2.getMonth() + 1)+"/"+picker2.getYear());
+//            }
+//        });
+
         startActivity(intent);
+
+
+
     }
 }
